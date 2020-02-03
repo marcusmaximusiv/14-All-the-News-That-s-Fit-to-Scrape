9f95b2f7c06d0a06ab83a82bd68ac6d6c84b0e5c
Sterrenberg, Mark <mark.allen.sterrenberg@gmail.com>
2020-02-03 01:22:45 -0600

    made corrections to server.js to deploy to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var db = require("../../../../../../models");
+var db = require("./models");
 var PORT = 3000;
 var app = express();
 app.use(logger("dev"));
@@ -14,43 +14,12 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.nytimes.com").then(function (response) {
+    axios.get("https://www.nytimes.com").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
-
-        console.log($);
-
-        // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function (i, element) {
-            // Save an empty result object
-            var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this)
-                .children("a")
-                .text();
-
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-
-            // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, log it
-                    console.log(err);
-                });
-        });
-
-        // Send a message to the client
-        res.send("New Articles Scraped");
+        console.log(response.data);
+    });
+    app.listen(PORT, function () {
+        console.log("App running on port " + PORT + "!");
     });
 })
-
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
